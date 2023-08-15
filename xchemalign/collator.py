dfb8f2c3067ced8c717e0781915bfc537282f1d8
jfi22496 <conor.wild@sky.com>
2023-08-15 17:23:42 +0100

    Debuging collator

--- xchemalign/collator.py
+++ xchemalign/collator.py
@@ -388,7 +388,6 @@ class Collator:
                         }
                     data[Constants.META_XTAL_FILES] = f_data
 
-        print(data)
 
         self.logger.info("validator handled {} rows from database, {} were valid".format(count, processed))
         if num_mtz_files < num_pdb_files:
