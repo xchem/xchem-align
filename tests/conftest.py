2ce710867957ecb58e9a24f2ad4500023c66c2f8
jfi22496 <conor.wild@sky.com>
2023-08-24 12:07:51 +0100

    Added some testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import shutil
 
 import pytest
 
+
 @pytest.fixture(scope="session")
 def constants():
     class Constants:
@@ -17,11 +18,16 @@ def constants():
         TEST_DIR = "test-data/outputs"
         UPLOAD_1_DIR = "test-data/outputs/upload_1"
         UPLOAD_2_DIR = "test-data/outputs/upload_2"
+        METADATA_FILE = "metadata_collator.yaml"
 
     return Constants()
 
+
 @pytest.fixture(scope="session")
 def test_data_dir(constants):
+    return Path(constants.TEST_DATA_DIR)
+
+
 @pytest.fixture(scope="session")
 def test_dir(constants):
     path = Path(constants.TEST_DIR)
@@ -34,19 +40,49 @@ def test_dir(constants):
 
 @pytest.fixture(scope="session")
 def upload_1_dir(constants, test_dir):
-    ...
+    path = Path(constants.UPLOAD_1_DIR)
+    if not path.exists():
+        os.mkdir(path)
+
+    return path
 
 
 @pytest.fixture(scope="session")
 def upload_2_dir(constants, test_dir):
-    ...
+    path = Path(constants.UPLOAD_2_DIR)
+    if not path.exists():
+        os.mkdir(path)
+
+    return path
 
 
 @pytest.fixture(scope="session")
 def upload_1_data_dir(constants, test_dir):
-    ...
+    path = Path(constants.INPUT_1_DIR)
+    return path
 
 
 @pytest.fixture(scope="session")
 def upload_2_data_dir(constants, test_dir):
-    ...
+    path = Path(constants.INPUT_2_DIR)
+    return path
+
+@pytest.fixture(scope="session")
+def config_1_file(constants, ):
+    path = Path(constants.CONFIG_1_FILE)
+    return path
+
+@pytest.fixture(scope="session")
+def config_2_file(constants, ):
+    path = Path(constants.CONFIG_2_FILE)
+    return path
+
+@pytest.fixture(scope="session")
+def assemblies_file(constants, ):
+    path = Path(constants.ASSEMBLIES_FILE)
+    return path
+
+@pytest.fixture(scope="session")
+def xtalforms_file(constants, ):
+    path = Path(constants.XTAFLORMS_FILE)
+    return path
