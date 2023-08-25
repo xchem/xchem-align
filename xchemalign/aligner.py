219999d671fab3fa77f5f13e3c8bd732e415c665
jfi22496 <conor.wild@sky.com>
2023-08-25 17:11:01 +0100

    Added manual refernce file to test set and fixed aligner metadata errors

--- xchemalign/aligner.py
+++ xchemalign/aligner.py
@@ -598,10 +598,6 @@ class Aligner:
                 crystal_output[canonical_site_id] = aligned_files
 
         new_meta[Constants.META_TRANSFORMS] = {}
-        # print(fs_model.reference_alignments)
-        # print(new_meta[Constants.META_REFERENCE_ALIGNMENTS])
-        # if len(fs_model.reference_alignments) > 1:
-        #     raise Exception
 
         ## Get the observation to conformer site transforms
         ligand_neighbourhood_transforms = read_yaml(fs_model.ligand_neighbourhood_transforms)
