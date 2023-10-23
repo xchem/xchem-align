3a6c19404f257ed45819fcf687f41fc637a6e808
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2023-10-23 12:53:56 +0100

    New crystalform and assemblies documentation

--- scripts/align.py
+++ scripts/align.py
@@ -11,6 +11,8 @@ def main():
     parser.add_argument("-d", "--version-dir", required=True, help="Path to version dir")
     parser.add_argument("-m", "--metadata_file", default=Constants.METADATA_XTAL_FILENAME, help="Metadata YAML file")
     parser.add_argument("-x", "--xtalforms", help="Crystal forms YAML file")
+    parser.add_argument("-a", "--assemblies", help="Crystal forms YAML file")
+
     parser.add_argument("-l", "--log-file", help="File to write logs to")
     parser.add_argument("--log-level", type=int, default=0, help="Logging level")
     parser.add_argument("--validate", action="store_true", help="Only perform validation")
@@ -20,7 +22,7 @@ def main():
 
     logger = utils.Logger(logfile=args.log_file, level=args.log_level)
 
-    a = Aligner(args.version_dir, args.metadata_file, args.xtalforms, logger=logger)
+    a = Aligner(args.version_dir, args.metadata_file, args.xtalforms, args.assemblies, logger=logger)
     num_errors, num_warnings = a.validate()
 
     if not args.validate:
