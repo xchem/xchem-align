0fdc42498aa6965a89108a392a637bc6cb349261
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2025-03-27 16:13:50 +0000

    ø

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,7 +49,7 @@ def test_dir(constants):
     return path
 
 @pytest.fixture(scope="session")
-def uploads_dir(constants, ):
+def uploads_dir(constants, test_dir):
     os.mkdir(Path(constants.TEST_DIR) / constants.VERSION_DIR)
     os.mkdir(Path(constants.TEST_DIR) / constants.VERSION_DIR / 'upload_1')
     os.mkdir(Path(constants.TEST_DIR) / constants.VERSION_DIR / 'upload_2')
