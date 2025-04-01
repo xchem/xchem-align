a8c757f9cc0249e2b62af316e406ffa1083b75b8
Kalev Takkis <ktakkis@informaticsmatters.com>
2025-04-01 10:38:00 +0100

    fix: removed versioning and main() code

--- src/ligand_neighbourhood_alignment/__init__.py
+++ src/ligand_neighbourhood_alignment/__init__.py
@@ -1,6 +1,6 @@
-from importlib.metadata import version
+# from importlib.metadata import version
 
-__version__ = version("ligand_neighbourhood_alignment")
-del version
+# __version__ = version("ligand_neighbourhood_alignment")
+# del version
 
-__all__ = ["__version__"]
+# __all__ = ["__version__"]
