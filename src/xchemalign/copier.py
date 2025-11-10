525385839ff1a2a5cd190560aaa1c6bba29d8a30
Tim Dudgeon <tdudgeon@informaticsmatters.com>
2025-11-10 12:20:27 +0000

    more copy dataprocessing files

--- src/xchemalign/copier.py
+++ src/xchemalign/copier.py
@@ -227,17 +227,17 @@ class Copier:
                     self._log_warning("Ligand PDB file " + filename + " not copied for crystal " + xtal_name)
             dp_log = row[Constants.SOAKDB_COL_DATA_PROCESSING_PATH_TO_LOGFILE]
             if dp_log and dp_log != 'None':
-                print('copying', str(dp_log))
+                self.logger.info('copying', str(dp_log))
                 num_files += self.copy_file_and_log(
                     xtal_name, Constants.SOAKDB_COL_DATA_PROCESSING_PATH_TO_LOGFILE, dp_log, xtal_dir_path
                 )
                 dp_log_p = Path()
                 stats_cif_p = dp_log_p.parent / 'xia2.mmcif.bz2'
                 if stats_cif_p.is_file():
-                    print('copying', str(stats_cif_p))
+                    self.logger.info('copying', str(stats_cif_p))
                     num_files += self.copy_file_and_log(xtal_name, 'xia2.mmcif.bz2', str(dp_log_p), xtal_dir_path)
                 else:
-                    print(str(stats_cif_p), 'not found')
+                    self.logger.info(str(stats_cif_p), 'not found')
             else:
                 self._log_warning("Data processing logfile not defined for crystal " + xtal_name)
 
