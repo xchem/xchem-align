2e4ba7a848344d0749da5125cb0225a428a8ff35
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-02-03 18:11:21 +0000

    Clearer map alignment errors

--- src/ligand_neighbourhood_alignment/xtalform_assignment.py
+++ src/ligand_neighbourhood_alignment/xtalform_assignment.py
@@ -63,7 +63,7 @@ def get_xtalform_chain_mapping(ref, mov, xtalform_protein_chains):
     # Assign each movin chain its closest ref chain
     assignments = {}
     min_distances = {}
-    for ref_chain in assignments:
+    for ref_chain in xtalform_protein_chains:
         closest_chain = min(assignments[ref_chain], key=lambda x: assignments[ref_chain][x])
         min_distances[ref_chain] = distances[ref_chain][closest_chain]
         assignments[ref_chain] = closest_chain
@@ -95,7 +95,8 @@ def _get_closest_xtalform(xtalforms: dict[str, dt.XtalForm], structure, structur
 
         # Check they have the same protein chains
         xtalform_protein_chains = [
-            _chain for xtalform_assembly in xtalform.assemblies.values() for _chain in xtalform_assembly.chains
+            _chain for xtalform_assembly in xtalform.assemblies.values() 
+            for _chain in xtalform_assembly.chains
         ]
         dataset_protein_chains = _get_dataset_protein_chains(structure)
 
