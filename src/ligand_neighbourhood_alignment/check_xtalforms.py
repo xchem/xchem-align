4517ab47a89db1f8a0f6ee7b41b1f9acb099c700
ConorFWild <41680328+ConorFWild@users.noreply.github.com>
2026-03-13 11:07:56 +0000

    New best assignment algorithm

---
+++ src/ligand_neighbourhood_alignment/check_xtalforms.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def _check_xtalforms(xtalforms, structures):
+    """
+    For every crystalform, check every other crystalform, and raise an exception if the unit cell is within tolderance
+    and it shares the same spacegroup
+    """
+    identical_crystalforms = []
+    for ref_xtalform_id, ref_xtalform in xtalforms.items():
+        ref_structure = structures[ref_xtalform.reference]
+        ref_spacegroup = ref_structure.spacegroup_hm
+        ref_structure_cell = ref_structure.cell
+        for mov_xtalform_id, mov_xtalform in xtalforms.items():
+            mov_structure = structures[mov_xtalform.reference]
+            mov_spacegroup = mov_structure.spacegroup_hm
+            mov_structure_cell = mov_structure.cell
+
+            if mov_spacegroup != ref_spacegroup:
+                continue
+
+            # Check deltas
+            deltas = np.array(
+            [
+                mov_structure_cell.a / ref_structure_cell.a,
+                mov_structure_cell.b / ref_structure_cell.b,
+                mov_structure_cell.c / ref_structure_cell.c,
+                mov_structure_cell.alpha / ref_structure_cell.alpha,
+                mov_structure_cell.beta / ref_structure_cell.beta,
+                mov_structure_cell.gamma / ref_structure_cell.gamma,
+            ]
+            )
+            if np.any(deltas > 1.1) | np.any(deltas < 0.9):
+                continue
+
+            # If this code executes then the spacegroup is the same and the unit cell is close: raise 
+            # an Exception!
+            identical_crystalform = set([ref_xtalform_id, mov_xtalform_id])
+            if identical_crystalform not in identical_crystalforms:
+                identical_crystalforms.append(identical_crystalform)
+        
+    if len(identical_crystalforms) != 0:
+        exception = f'There were identical crystalforms present in the assemblies.yaml \n'
+        for identical_crystalform in identical_crystalforms:
+            exception += f'\t{identical_crystalform[0]} is identical to {identical_crystalform[1]}\n'
+        exception += 'Please remove the redundancy and rerun!'
+        raise Exception(
+                exception
+            )
